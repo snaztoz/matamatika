5bc30bc35f7a0092e40e543172b7d649a9baf2df
snaztoz <hafidhmn@gmail.com>
2020-07-05 12:26:00 +0700

    [Model] Add Question model and define some fields

---
+++ database/migrations/2020_07_05_050544_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->timestamps();
+            
+            // mungkin mekanisme untuk penyimpanan tulisan dapat
+            // diubah nanti
+            $table->string('content', 512);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
