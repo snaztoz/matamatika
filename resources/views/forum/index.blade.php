e8f38c6d5b211a84774c5e796953ed2a4c9077ad
snaztoz <hafidhmn@gmail.com>
2020-07-06 21:49:19 +0700

    [View] Refactor the forum pages template (make the sidebar has its own template)

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -50,19 +50,7 @@
 		</div>
 
 		<!-- Sidebar untuk pengumuman terkait kegiatan mentoring -->
-		<div class="col-4 d-none d-md-block">
-			<aside class="card">
-				<header class="card-header">
-					<h3>Mentoring</h3>
-				</header>
-
-				<content class="card-body">
-					<ol class="pl-4">
-						<li><a href="#">Kelas online untuk materi limit kelas XI</a></li>
-					</ol>
-				</content>
-			</aside>
-		</div>
+		@include ('forum.sidebar')
 	</div>
 </div>
 @endsection
---
+++ resources/views/forum/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="col-4 d-none d-md-block">
+	<aside class="card">
+		<header class="card-header">
+			<h3>Mentoring</h3>
+		</header>
+
+		<content class="card-body">
+			<ol class="pl-4">
+				<li><a href="#">Kelas online untuk materi limit kelas XI</a></li>
+			</ol>
+		</content>
+	</aside>
+</div>
