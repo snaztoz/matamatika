8d024f5e6174394fb2bcd4d76e8241592f4ab7a2
snaztoz <hafidhmn@gmail.com>
2020-07-06 19:50:06 +0700

    [View] Add title input bar in Question creation form

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -16,15 +16,29 @@
 
 				<content class="card-body">
 					<h4 class="mb-4">Ada yang ingin ditanyakan? Tuliskan pertanyaanmu!</h4>
-					<div id="question-typing-area">
-						<!-- displaying text editor -->
-					</div>
 
 					<form method="POST" action="{{ route('forum.store') }}" id="question-form">
 						@csrf
+
+						<div class="form-group container">
+							<label for="title" class="">Judul pertanyaan</label>
+							<input name="title" value="{{ old('title') }}" placeholder="judul akan ditampilkan di halaman utama forum" class="form-control">	
+						</div>						
+
+						<div class="form-group container">
+							<label for="question-typing-area">Isi pertanyaan</label>
+							<div id="question-typing-area">
+								<!-- displaying text editor -->
+							</div>
+						</div>
+
 						<!-- hidden textarea -->
 						<textarea id="question-hidden-textarea" name="content" hidden></textarea>
-						<input id="question-submit-button" class="btn btn-primary mt-4" type="submit" value="Unggah Pertanyaanmu">
+
+						<div class="container">
+							<input id="question-submit-button" class="btn btn-primary mt-2" type="submit" value="Unggah Pertanyaanmu">	
+						</div>
+						
 					</form>
 				</content>
 			</main>
