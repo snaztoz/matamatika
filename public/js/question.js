15a98827813c9dd1a9a068ff890fbeab78c33a29
snaztoz <hafidhmn@gmail.com>
2020-07-06 11:08:49 +0700

    [Script] Add small fix to Question's script

--- public/js/question.js
+++ public/js/question.js
@@ -13,9 +13,10 @@ var editor = new Quill('#question-typing-area', {
 
 /**
  * Salin isi dari div #question-typing-area ke dalam
- * hidden textarea sebelum form disubmit.
+ * hidden textarea sebelum question-form disubmit.
  */
 $('#question-submit-button').click(function(event) {
-	$('#question-hidden-textarea').value = editor.root.innerHTML;
+	event.preventDefault();
+	$('#question-hidden-textarea').val(editor.root.innerHTML);
 	$('#question-form').submit();
 })
