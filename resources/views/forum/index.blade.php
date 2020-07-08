82e6b6bb6366f0380e3970813a11057cec9b52b0
snaztoz <hafidhmn@gmail.com>
2020-07-08 15:04:00 +0700

    [View] Add user images to forum's pages wireframe

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -3,7 +3,10 @@
 @section ('main')
 <article class="card mb-4">
 	<div class="card-body pb-2">
-		<p class="mb-2">user1537264, ditanyakan 5 hari yang lalu</p>
+		<div class="container pl-0 d-flex">
+			<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+			<p class="ml-2">user123421</p>
+		</div>
 		<h4>Jika terdapat f(x) = 5x, tentukan integral pertama dari f(x)</h4>
 		<p class="mb-2">0 jawaban</p>
 		<!-- hidden-link -->
@@ -13,7 +16,10 @@
 
 <article class="card mb-4">
 	<div class="card-body pb-2">
-		<p class="mb-2">Andi Andrew, ditanyakan 10 hari yang lalu</p>
+		<div class="container pl-0 d-flex">
+			<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+			<p class="ml-2">Andi Andrew</p>
+		</div>
 		<h4>Jika terdapat f(x) = 5x, tentukan integral pertama dari f(x)</h4>
 		<p class="mb-2">2 jawaban</p>
 		<!-- hidden-link -->
@@ -23,7 +29,10 @@
 
 <article class="card mb-4">
 	<div class="card-body pb-2">
-		<p class="mb-2">Kevin, ditanyakan 4 hari yang lalu</p>
+		<div class="container pl-0 d-flex">
+			<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+			<p class="ml-2">Kevin</p>
+		</div>
 		<h4>Jika terdapat f(x) = 5x, tentukan integral pertama dari f(x)</h4>
 		<p class="mb-2">5 jawaban, <u class="text-success">Terjawab</u></p>
 		<!-- hidden-link -->
--- resources/views/forum/show.blade.php
+++ resources/views/forum/show.blade.php
@@ -31,7 +31,10 @@
 		<section class="mt-4">
 			<!-- Individual answer -->
 			<div class="mb-4">
-				<p class="mb-3">user3412312</p>
+				<div class="container pl-0 d-flex">
+					<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+					<p class="ml-2">user3412312</p>
+				</div>
 				<p>Jawabannya adalah x = 5</p>
 				<div class="d-flex pb-2">
 					<small class="d-block mr-4">Balas</small>
@@ -39,13 +42,19 @@
 				</div>
 
 				<!-- Answer's replies -->
-				<section class="container mt-2">
-					<div class="p-1 pl-3 pr-2 mb-3 site-border-left-yellow bg-light">
-						<p class="mb-2">user12341</p>
+				<section class="container site-border-left-yellow site-bg-grey pt-2 mt-2 ml-3">
+					<div class="p-1 pl-2 pr-2 mb-3">
+						<div class="container pl-0 d-flex">
+							<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+							<p class="ml-2">user123421</p>
+						</div>
 						<p>Bukannya 7 ya?</p>
 					</div>
-					<div class="p-1 pl-3 pr-2 mb-3 site-border-left-yellow bg-light">
-						<p class="mb-2">user3412312</p>
+					<div class="p-1 pl-2 pr-2 mb-3">
+						<div class="container pl-0 d-flex">
+							<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+							<p class="ml-2">user3412312</p>
+						</div>
 						<p>Oiya deng bener juga :) . Maaf salah</p>
 					</div>
 				</section>
@@ -53,7 +62,10 @@
 
 			<!-- Individual answer -->
 			<div>
-				<p class="mb-3">user12543</p>
+				<div class="container pl-0 d-flex">
+					<img class="site-shape-circle" src="{{ asset('storage/images/me.jpg') }}" width="30px" height="30px" alt="user">
+					<p class="ml-2">user12543</p>
+				</div>
 				<p>7 gan jawabannya</p>
 				<div class="d-flex pb-2">
 					<small class="d-block mr-4">Balas</small>
