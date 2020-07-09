a8e79baa8a6c7b7b342f5c2515e110b1e2bd5757
snaztoz <hafidhmn@gmail.com>
2020-07-09 20:12:30 +0700

    [View] Add toggler for answer's replies section, and add small change to other parts

--- public/js/answer.js
+++ public/js/answer.js
@@ -1,3 +1,6 @@
+/* Editor untuk menulis jawaban terhadap pertanyaan 
+ * terkait.
+ */
 var answerEditor = new Quill('#answer-typing-area', {
 	modules: {
 		toolbar: {
@@ -9,4 +12,21 @@ var answerEditor = new Quill('#answer-typing-area', {
 	},
 	placeholder: 'Tulis jawabanmu di sini',
 	theme: 'snow'
+});
+
+/* Editor untuk menuliskan balasan terhadap jawaban-jawaban
+ * yang ada.
+ * Mengingat satu editor hanya dapat dipasang di satu object,
+ * maka penggunaan editor ini akan sedikit berbeda. 
+ */
+var replyEditor;
+
+/* Membuka dan menutup bagian balasan dari jawaban
+ * tertentu.
+ */
+$('.site-replies-show-button').each(function() {
+	$(this).click(event => {
+		event.preventDefault();
+		$(this).siblings('.site-replies').toggleClass('d-block');
+	});
 });
--- resources/views/forum/show.blade.php
+++ resources/views/forum/show.blade.php
@@ -28,7 +28,7 @@
 		<h4>2 jawaban</h4>
 
 		<!-- All answers -->
-		<section class="mt-4">
+		<div class="mt-4">
 			<!-- Individual answer -->
 			<div class="mb-4">
 				<div class="container pl-0 d-flex">
@@ -36,45 +36,57 @@
 					<p class="ml-2">user3412312</p>
 				</div>
 				<p>Jawabannya adalah x = 5</p>
-				<div class="d-flex pb-2">
-					<small class="d-block mr-4">Balas</small>
-					<small class="d-block">2 balasan</small>
-				</div>
-
+				
+				<a href="#" class="site-replies-show-button d-inline-block text-dark ">
+					<small class="d-block">2 balasan <span class="small">&#9660;</span></small>
+				</a>
+			
 				<!-- Answer's replies -->
-				<section class="container site-border-left-yellow site-bg-grey pt-2 mt-2 ml-3">
-					<div class="p-1 pl-2 pr-2 mb-3">
+				<section class="site-replies d-none container site-border-left-red site-bg-grey pt-2 pb-2 mt-2 ml-3">
+					<div class="p-0 pl-2 pr-2 mb-4">
 						<div class="container pl-0 d-flex">
 							<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
 							<p class="ml-2">user123421</p>
 						</div>
 						<p>Bukannya 7 ya?</p>
 					</div>
-					<div class="p-1 pl-2 pr-2 mb-3">
+					<div class="p-0 pl-2 pr-2 mb-4">
 						<div class="container pl-0 d-flex">
 							<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
 							<p class="ml-2">user3412312</p>
 						</div>
 						<p>Oiya deng bener juga :) . Maaf salah</p>
 					</div>
+
+					<!-- Replying box -->
+					<div class="p-0 pl-2 pr-2">
+						<div class="reply-typing-area"></div>
+						<button id="reply-to-2432" class="btn btn-secondary">Balas</button>
+					</div>
 				</section>
 			</div>
 
 			<!-- Individual answer -->
-			<div>
+			<div class="mb-4">
 				<div class="container pl-0 d-flex">
 					<img class="site-shape-circle" src="{{ asset('storage/images/me.jpg') }}" width="30px" height="30px" alt="user">
 					<p class="ml-2">user12543</p>
 				</div>
 				<p>7 gan jawabannya</p>
-				<div class="d-flex pb-2">
-					<small class="d-block mr-4">Balas</small>
-				</div>
+				<a href="#" class="site-replies-show-button d-inline-block text-dark ">
+					<small class="d-block">0 balasan <span class="small">&#9660;</span></small>
+				</a>
 
-				<section class="container mt-2"></section>
+				<section class="site-replies d-none container site-border-left-red site-bg-grey pt-2 pb-2 mt-2 ml-3">
+					<!-- Replying box -->
+					<div class="p-0 pl-2 pr-2">
+						<div class="reply-typing-area"></div>
+						<button id="reply-to-2432" class="btn btn-secondary">Balas</button>
+					</div>
+				</section>
 			</div>
 
-		</section>
+		</div>
 	</div>
 </section>
 @endsection
