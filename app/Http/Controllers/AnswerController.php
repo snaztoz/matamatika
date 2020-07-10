12059ca3c4f33a0287cd232312f6e0164cfa8e8e
snaztoz <hafidhmn@gmail.com>
2020-07-10 20:06:40 +0700

    [Controller & Route] Create AnswerController and assign shallow route for it with Forum's route

---
+++ app/Http/Controllers/AnswerController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Answer;
+use App\Question;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AnswerController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Question $forum, Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Answer $answer)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('forum', 'ForumController')->parameters([
 	'forum' => 'question'
 ]);
+
+Route::resource('forum.answers', 'AnswerController')->shallow();
