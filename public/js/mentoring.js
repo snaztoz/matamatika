89e3134e8715bbe88e17d92ee44ea299fca2865c
snaztoz <hafidhmn@gmail.com>
2020-07-16 22:36:59 +0700

    Add validation to 'create' page of mentoring
    - Create some error messages
    - Create validation mechanism
    - Create mechanism that will change every newline occurences
      in description into <br> tag
    - Modify desc typing area styles

---
+++ public/js/mentoring.js
@@ -0,0 +1,33 @@
+/**
+ * Mengubah ukuran dari typing area agar menyesuaikan
+ * dengan konten yang ada di dalamnya.
+ */
+$('#mentoring-desc-typing-area').on("input", function() {
+	this.style.height = '2.5rem';
+	this.style.height = `${this.scrollHeight}px`;
+});
+
+/**
+ * Validasi form ketika tombol submit ditekan.
+ * Juga mengganti setiap karakter 'newline' yang
+ * ditemui di desc typing area menjadi <br>.
+ */
+$('#mentoring-submit-button').click(function(event) {
+	if ($('#mentoring-title-typing-area').val().trim() === '') {
+		event.preventDefault();
+		$('#title-empty-message').addClass('d-inline');
+		return;
+	}
+
+	if ($('#mentoring-desc-typing-area').val().trim() === '') {
+		event.preventDefault();
+		$('#desc-empty-message').addClass('d-inline');
+		return;
+	}
+
+	event.preventDefault();
+	$('#mentoring-desc-typing-area').val(
+		$('#mentoring-desc-typing-area').val().split('\n').join('<br>')
+	);
+	console.log($('#mentoring-desc-typing-area').val());
+});
--- resources/views/mentoring/create.blade.php
+++ resources/views/mentoring/create.blade.php
@@ -1,5 +1,9 @@
 @extends ('mentoring.base')
 
+@section ('scripts')
+<script src="{{ asset('js/mentoring.js' )}}" defer></script>
+@endsection
+
 @section ('main')
 <h4>Buat Mentoring Baru</h4>
 
@@ -9,19 +13,19 @@
 		
 		<div class="form-group container">
 			<label for="title">Judul Mentoring</label>
-			<input name="title" value="{{ old('title') }}" placeholder="judul akan ditampilkan di halaman utama forum" class="form-control" required>	
+			<input name="title" value="{{ old('title') }}" placeholder="judul akan ditampilkan di halaman utama forum" id="mentoring-title-typing-area" class="form-control" required>
+			<small id="title-empty-message" class="text-danger d-none">*Judul tidak boleh kosong</small>
 		</div>						
 
 		<div class="form-group container">
-			<label for="question-typing-area">Deskripsi Mentoring</label>
-			<textarea name="content" id="mentoring-desc-textarea" class="form-control w-100" placeholder="tuliskan deskripsi kegiatan mentoring yang akan dibuat"></textarea>
-			<small id="question-typing-area-empty-message" class="text-danger d-none">*Isi pertanyaan tidak boleh kosong</small>
+			<label for="mentoring-desc-typing-area">Deskripsi Mentoring</label>
+			<textarea name="content" id="mentoring-desc-typing-area" class="mentoring-desc-typing-area form-control w-100" placeholder="tuliskan deskripsi kegiatan mentoring yang akan dibuat"></textarea>
+			<small id="desc-empty-message" class="text-danger d-none">*Deskripsi tidak boleh kosong</small>
 		</div>
 
 		<div class="container">
 			<input id="mentoring-submit-button" class="btn btn-primary mt-2" type="submit" value="Buat Kegiatan Mentoring">	
 		</div>
-	
 	</form>
 </div>
 @endsection
