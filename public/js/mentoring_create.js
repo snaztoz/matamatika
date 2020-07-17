fd9d7d68f2e8345a0561d1d6d0d7a2a9946a0244
snaztoz <hafidhmn@gmail.com>
2020-07-17 22:12:45 +0700

    Add change to mentoring's show page
    - Add new parts to the page
    - Create scripts to it

--- public/js/mentoring_create.js
+++ public/js/mentoring_create.js
@@ -1,7 +1,8 @@
-let mentoringCreationEditor = new Quill('#mentoring-email-typing-area', {
+let mentoringEmailEditor = new Quill('#mentoring-email-typing-area', {
 	modules: {
 		toolbar: {
 			container: [
+				[{'header': 1}, {'header': 2}],
 				['bold', 'italic', 'underline'],
 				['link']
 			],
@@ -10,3 +11,22 @@ let mentoringCreationEditor = new Quill('#mentoring-email-typing-area', {
 	placeholder: 'Tuliskan email',
 	theme: 'snow'
 });
+
+$('#mentoring-email-submit').click(function(event) {
+	// mencegah isi email kosong
+	if (!mentoringEmailEditor.getText().trim().length) {
+		event.preventDefault();
+		$('#mentoring-email-typing-area-empty-message').addClass('d-inline');
+		return;
+	}
+
+	$('#mentoring-email-hidden-textarea')
+			.val(mentoringEmailEditor.root.innerHTML);
+});
+
+/**
+ * Mengisi text editor ketika page pertama kali dimuat 
+ * dengan isi dari email yang terakhir kali dikirim.
+ */
+mentoringEmailEditor.root.innerHTML = 
+		$('#mentoring-email-hidden-textarea').val();
--- resources/views/mentoring/show.blade.php
+++ resources/views/mentoring/show.blade.php
@@ -10,18 +10,38 @@
 <h3>{{ $mentoring->title }}</h3>
 
 <div class="site-border-top-grey mt-3 p-3">
-	<form>
+	<form method="POST" action="#">
+		@csrf
 		<div class="form-group">
 			<label for="mentoring-email-typing-area">Tulis Email</label>
 			<div id="mentoring-email-typing-area">
 				<!-- displaying text editor -->
 			</div>
-			<small id="mentoring-email-typing-area-empty-message" class="text-danger d-none">*Isi pertanyaan tidak boleh kosong</small>
+			<small id="mentoring-email-typing-area-empty-message" class="text-danger d-none">*Isi email tidak boleh kosong</small>
 		</div>
 
 		<textarea id="mentoring-email-hidden-textarea" name="mail" hidden>{{ $latest_mail }}</textarea>
 
-		<input type="submit" value="Kirim Email" class="btn btn-primary">
+		<input type="submit" value="Kirim Email*" id="mentoring-email-submit" class="btn btn-primary">
+		<small class="d-block mt-2 text-info">
+				*Email terbaru yang akan dikirim ke semua peserta yang tergabung
+				dengan kegiatan ini</small>
 	</form>		
+</div>
+
+<div class="mt-4 pt-2">
+	<form method="POST" action="{{ route('mentoring.destroy', ['mentoring' => $mentoring]) }}" 
+			class="container">
+		@csrf
+		@method('DELETE')
+		<div class="form-group container rounded col-md-6 p-3">
+			<label class="text-center">Tandai kegiatan mentoring menjadi sudah selesai:</label>
+			<div class="border-top border-light d-flex flex-column align-items-center">
+				<input type="submit" value="Selesai" class="btn btn-info mb-2 text-white">
+				<small class="d-block text-danger text-center">PERHATIAN: kegiatan yang sudah selesai 
+						tidak dapat dikembalikan lagi</small>	
+			</div>
+		</div>
+	</form>
 </div>
 @endsection
