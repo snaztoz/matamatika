2395428b759f026ec4c2ac7a02afa9e0167899d7
snaztoz <hafidhmn@gmail.com>
2020-07-19 21:13:14 +0700

    Create Mailable for mentoring updates
    - Create a new Mailable class that will be used in sending
      email to a mentoring's participants

---
+++ app/Mail/MentoringUpdate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\MentoringMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MentoringUpdate extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Kegiatan mentoring terkait yang akan dikirimkan email.
+     */
+    public $mentoring_mail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(MentoringMail $mail)
+    {
+        $this->mentoring_mail = $mail;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mails.mentoring_update');
+    }
+}
