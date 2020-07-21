948faeb8c9c93adbbd23c0e38369798cf3389d38
snaztoz <hafidhmn@gmail.com>
2020-07-21 22:25:07 +0700

    Add 'Add New' button in mentoring index
    - Add button that will redirect to add new mentoring events when
      the current user is the admin.

--- resources/views/mentoring/index.blade.php
+++ resources/views/mentoring/index.blade.php
@@ -2,7 +2,12 @@
 
 @section ('main')
 
-<h4>Kegiatan Mentoring</h4>
+<div class="d-flex px-2 py-1 align-items-center">
+	<h4 class="">Kegiatan Mentoring</h4>
+	@if (Auth::check() && Auth::user()->name === 'admin')
+	<p class="ml-auto mb-0"><a class="btn btn-primary" href="{{ route('mentoring.create') }}">Buat Baru+</a></p>
+	@endif	
+</div>
 
 <div class="site-border-top-grey mt-3 p-3">
 @forelse ($mentorings as $mentoring)
