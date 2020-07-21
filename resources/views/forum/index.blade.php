6248631655ef01cb401b9decfe195eb503057e0d
snaztoz <hafidhmn@gmail.com>
2020-07-21 22:42:27 +0700

    Add small change to forum index page
    - Add forum section header.
    - Add button that will be redirecting user to new question
      creation page.

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -2,22 +2,32 @@
 
 @section ('main')
 
+<header class="d-lg-flex align-items-center p-2">
+	<h3>Forum Tanya Jawab</h3>
+	<p class="m-0 ml-auto">
+		<a class="btn btn-primary" href="{{ route('forum.create') }}">Tulis Pertanyaan</a></p>
+</header>
+
+<section class="site-border-top-grey p-2 py-3">
 @forelse ($questions as $question)
-<article class="card mb-4">
-	<div class="card-body pb-2">
-		<div class="container pl-0 d-flex">
-			<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
-			<p class="ml-2">{{ $question->user->name }}</p>
+	<article class="card mb-4">
+		<div class="card-body pb-2">
+			<div class="container pl-0 d-flex">
+				<img class="site-shape-circle" src="{{ asset('storage/images/default_user.png') }}" width="30px" height="30px" alt="user">
+				<p class="ml-2">{{ $question->user->name }}</p>
+			</div>
+			<h4>{{ $question->title }}</h4>
+			<p class="mb-2">{{ $question->answers->count() }} jawaban</p>
+			<!-- hidden-link -->
+			<a class="stretched-link" href="{{ route('forum.show', ['question' => $question]) }}">Lihat pertanyaan</a>
 		</div>
-		<h4>{{ $question->title }}</h4>
-		<p class="mb-2">{{ $question->answers->count() }} jawaban</p>
-		<!-- hidden-link -->
-		<a class="stretched-link" href="{{ route('forum.show', ['question' => $question]) }}">Lihat pertanyaan</a>
-	</div>
-</article>
+	</article>
+
 @empty
-	<h4>Wah, belum ada pertanyaan nih</h4>
-@endforelse
+	<h5><i>Wah, belum ada pertanyaan nih</i></h5>
+
+@endforelse	
+</section>
 
 <footer class="card-footer bg-white d-flex justify-content-center">
 	{{ $questions->links() }}
